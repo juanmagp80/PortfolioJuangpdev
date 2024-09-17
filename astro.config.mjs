d519c082aa38dbab5650dc5095cc73dd47db60d6
Juanma <juangpdev@gmail.com>
2024-09-17 15:28:48 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt(), react()],
+
+  integrations: [tailwind(), robotsTxt(), react(),],
   site: 'https://porfolio.dev/'
 })
